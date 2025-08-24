acb2266608eefc8088832a4a9d80327ea728e488
hasan <hasankassab375@gmail.com>
2025-08-24 16:56:30 +0300

    Add Remote Controller

---
+++ Controllers/RemoteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestUploadFiles.Controllers
+{
+    public class RemoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
